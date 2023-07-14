f4e7836acc37f7215cf6c285dccd9459f78fcd68
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+200920082007@users.noreply.github.com>
2023-07-14 15:00:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,23 +10,20 @@ logging.basicConfig(level=logging.ERROR)
 CHANNELS = [int(CHANNEL) for CHANNEL in environ.get("CHANNELS", None).split()]       
 AuthChat = filters.chat(CHANNELS) if CHANNELS else (filters.group | filters.channel)         
 User     = Client(name = "AcceptUser", api_id=environ.get("API_ID"), api_hash=environ.get("API_HASH"), session_string = environ.get("SESSION"))
-User1     = Client(name = "Accept", api_id=environ.get("API_ID"), api_hash=environ.get("API_HASH"), session_string = environ.get("SESSION1"))
 
 @User.on_message(filters.command(["run", "approve", "start"], [".", "/"]) & filters.private)                     
 async def approve(client: User, message: Message):
-    Id = -1001845218819
+    Id = message.from_user.id
     NJ = message.from_user.id
     await message.delete(True)
  
     try:
        while True: # create loop is better techniq 🙃
            try:
-               await User.approve_all_chat_join_requests(Id) 
-               await User1.approve_all_chat_join_requests(Id)
+               await User.approve_all_chat_join_requests(Id)
            except FloodWait as t:
                asyncio.sleep(t.value)
-               await User.approve_all_chat_join_requests(Id) 
-               await User1.approve_all_chat_join_requests(Id) 
+               await User.approve_all_chat_join_requests(Id)
            except Exception as e:
                logging.error(str(e))
     except FloodWait as s:
@@ -34,11 +31,9 @@ async def approve(client: User, message: Message):
         while True:
            try:
                await User.approve_all_chat_join_requests(Id)     
-               await User1.approve_all_chat_join_requests(Id) 
            except FloodWait as t:
                asyncio.sleep(t.value)
                await User.approve_all_chat_join_requests(Id) 
-               await User1.approve_all_chat_join_requests(Id) 
            except Exception as e:
                logging.error(str(e))
 
@@ -48,12 +43,7 @@ async def approve(client: User, message: Message):
 
 
 logging.info("Bot Started....")
-apps = [User,User1]
-    for app in apps:
-        app.start()
-    idle()
-    for app in apps:
-        app.stop()
+User.run()
 
 
 
